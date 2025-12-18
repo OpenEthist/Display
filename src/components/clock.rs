5f81a269ad67353d7f8b05ac3f64886468e00c1f
Taxrosdev <taxrosdev@mailbox.org>
2025-12-18 23:02:22 +1300

    feat: 24 hour time, seconds

--- src/components/clock.rs
+++ src/components/clock.rs
@@ -1,15 +1,16 @@
 use chrono::Timelike;
-use iced::Font;
-use iced::font::Weight;
-use iced::time::{self, milliseconds, seconds};
-use iced::widget::text;
-use iced::{Element, Subscription};
+use iced::{
+    Element, Subscription,
+    font::Weight,
+    time::{self, milliseconds},
+    widget::{Row, Text},
+};
 
+use crate::settings::SETTINGS;
 use crate::theme::get_font;
 
 pub struct Clock {
     now: chrono::DateTime<chrono::Local>,
-    seconds: bool,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -17,11 +18,18 @@ pub enum ClockMessage {
     Tick(chrono::DateTime<chrono::Local>),
 }
 
+fn text<'a>(str: impl iced::widget::text::IntoFragment<'a>) -> Element<'a, ClockMessage> {
+    let font_size = 100;
+    let mut font = get_font();
+    font.weight = Weight::Bold;
+
+    Text::new(str).font(font).size(font_size).into()
+}
+
 impl Clock {
     pub fn new() -> Self {
         Self {
             now: chrono::offset::Local::now(),
-            seconds: false,
         }
     }
 
@@ -36,28 +44,42 @@ impl Clock {
     }
 
     pub fn view(&self) -> Element<'_, ClockMessage> {
-        let (meridiem, hour) = self.now.hour12();
+        let mut result = Row::new();
+        let show_seconds = SETTINGS.clock.seconds;
+        let hide_meridiem = SETTINGS.clock.hour_24;
+
+        // Hour (and meridiem)
+        let (meridiem, hour) = if hide_meridiem {
+            let hour = self.now.hour();
+            (false, hour)
+        } else {
+            self.now.hour12()
+        };
+
+        // Minute
         let minute = self.now.minute();
+        let minute_text = format!("{minute:0>2}");
+        result = result.extend([text(hour), text(":"), text(minute_text)]);
 
-        let meridiem_text = if meridiem { "PM" } else { "AM" };
+        // Seconds
+        if show_seconds {
+            let second = self.now.second();
+            result = result.extend([text(":"), text(second)])
+        }
 
-        let mut font = get_font();
-        font.weight = Weight::Bold;
+        // AM/PM
+        if !hide_meridiem {
+            let meridiem_text = if meridiem { text(" PM") } else { text(" AM") };
+            result = result.extend([meridiem_text]);
+        }
 
-        let text = text!("{hour}:{minute:0>2} {meridiem_text}")
-            .font(font)
-            .size(100);
-
-        text.into()
+        result.into()
     }
 
     pub fn subscription(&self) -> Subscription<ClockMessage> {
-        // Just under a second/minute intentionally
-        // Stops rare "double second/minute" change.
-        let time = match self.seconds {
-            true => milliseconds(950),
-            false => seconds(59),
-        };
+        // Just under a secondintentionally
+        // Stops rare "double second" change.
+        let time = milliseconds(950);
 
         time::every(time).map(|_| ClockMessage::Tick(chrono::offset::Local::now()))
     }

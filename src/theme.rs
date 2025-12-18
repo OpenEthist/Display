b6fefd134133c0475ebcb967b4e095e2b5800b9c
Taxrosdev <taxrosdev@mailbox.org>
2025-12-18 22:21:12 +1300

    chore: remove dead variable

--- src/theme.rs
+++ src/theme.rs
@@ -3,7 +3,6 @@ use iced::{Font, Theme};
 use crate::settings::SETTINGS;
 
 pub const DEFAULT_THEME: Theme = Theme::CatppuccinMocha;
-const DEFAULT_FONT: &str = "Roboto";
 
 pub fn get_font() -> Font {
     Font::with_name(&SETTINGS.theme.font)
